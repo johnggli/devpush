1ecf6c892b0a779fb517d44c521152b2e1b8a962
John Emerson <john.carv.sousa@gmail.com>
2021-03-11 16:48:05 -0300

    feat: add home screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:devpush/screens/splash_screen/splash_screen.dart';
 import 'package:devpush/screens/login_screen/login_screen.dart';
+import 'package:devpush/screens/home_screen/home_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
   LoginScreen.routeName: (context) => LoginScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
 };
--- lib/screens/login_screen/components/welcome.dart
+++ lib/screens/login_screen/components/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:devpush/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:devpush/providers/auth_provider.dart';
@@ -6,32 +7,19 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var authProvider = Provider.of<AuthProvider>(context);
-    String picture = authProvider.picture;
+    // String picture = authProvider.picture;
     String name = authProvider.name;
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blue, width: 4),
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(picture ?? ''),
-            ),
-          ),
-        ),
-        const SizedBox(height: 24),
-        Text('Name: $name'),
+        Text('Bem vindo $name!'),
         const SizedBox(height: 48),
         ElevatedButton(
-          onPressed: () async {
-            await authProvider.logoutAction();
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, HomeScreen.routeName);
           },
-          child: const Text('Logout'),
+          child: const Text('Continuar'),
         ),
       ],
     );
