5a5e88231490d85cb1aeaa19a50344456bfa7ee0
John Emerson <john.carv.sousa@gmail.com>
2021-03-11 18:18:24 -0300

    feat: add splash screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/widgets.dart';
-import 'package:devpush/screens/splash_screen/splash_screen.dart';
 import 'package:devpush/screens/login_screen/login_screen.dart';
 import 'package:devpush/screens/home_screen/home_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  SplashScreen.routeName: (context) => SplashScreen(),
   LoginScreen.routeName: (context) => LoginScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
 };
