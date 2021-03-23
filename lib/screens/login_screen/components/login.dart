f6967d5a5d9f62700abcee0e6e9bfa1d37bf8884
John Emerson <john.carv.sousa@gmail.com>
2021-03-23 19:47:16 -0300

    feat: add url launcher

--- lib/screens/login_screen/components/login.dart
+++ lib/screens/login_screen/components/login.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:devpush/providers/auth_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Login extends StatelessWidget {
+  void _launchURL(String url) async =>
+      await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+
   @override
   Widget build(BuildContext context) {
     var authProvider = Provider.of<AuthProvider>(context);
@@ -18,6 +23,37 @@ class Login extends StatelessWidget {
           child: const Text('Login'),
         ),
         Text(errorMessage ?? ''),
+        SizedBox(height: 24),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              text: "By creating an account, you are agreeing to our\n",
+              style: Theme.of(context).textTheme.bodyText1,
+              children: [
+                TextSpan(
+                  text: "Terms & Conditions ",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      _launchURL(
+                          'https://sites.google.com/view/meteorrainapp/privacy-policy-kaomoji');
+                    },
+                ),
+                TextSpan(text: "and "),
+                TextSpan(
+                  text: "Privacy Policy! ",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      _launchURL('https://github.com/JohnEmerson1406');
+                    },
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
