4c43e4f5be34e64956353e83a75a79809b5c774f
John Emerson <john.carv.sousa@gmail.com>
2021-03-31 16:07:52 -0300

    feat: add firebase core and cloud firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,62 @@
 import 'package:devpush/providers/github_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:devpush/routes.dart';
 import 'package:devpush/screens/login_screen/login_screen.dart';
 import 'package:devpush/providers/auth_provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        // home: SplashScreen(title: 'Flutter Demo Home Page'),
-        initialRoute: LoginScreen.routeName,
-        routes: routes,
-      ),
+      child: FutureBuilder(
+          // Initialize FlutterFire:
+          future: _initialization,
+          builder: (context, snapshot) {
+            // Check for errors
+            if (snapshot.hasError) {
+              return MaterialApp(
+                home: Scaffold(
+                  body: Center(
+                    child: Column(
+                      children: [
+                        Text('Error!'),
+                        Text(snapshot.error.toString())
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+            // Show something whilst waiting for initialization to complete
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return MaterialApp(
+                home: Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              );
+            }
+            // Once complete, show the application
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              // home: SplashScreen(title: 'Flutter Demo Home Page'),
+              home: LoginScreen(),
+            );
+          }),
       providers: [
         ChangeNotifierProvider(create: (context) => AuthProvider()),
         ChangeNotifierProvider(create: (context) => GithubProvider()),
