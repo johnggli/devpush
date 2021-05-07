fb2937c53c68770fe6edb97e271438c83ccaf207
John Emerson <john.carv.sousa@gmail.com>
2021-05-06 22:38:56 -0300

    feat: add privacy policies to login screen

--- lib/screens/login_screen/components/login.dart
+++ lib/screens/login_screen/components/login.dart
@@ -1,15 +1,16 @@
+import 'package:devpush/core/app_colors.dart';
 import 'package:devpush/core/app_gradients.dart';
 import 'package:devpush/core/app_text_styles.dart';
 import 'package:devpush/core/app_images.dart';
-// import 'package:flutter/gestures.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:devpush/providers/auth_provider.dart';
-// import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Login extends StatelessWidget {
-  // void _launchURL(String url) async =>
-  //     await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+  void _launchURL(String url) async =>
+      await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
 
   @override
   Widget build(BuildContext context) {
@@ -43,8 +44,7 @@ class Login extends StatelessWidget {
             ),
           ),
           Padding(
-            padding:
-                const EdgeInsets.only(top: 28, left: 28, right: 28, bottom: 42),
+            padding: const EdgeInsets.all(18),
             child: Container(
               width: 310,
               child: Column(
@@ -58,7 +58,7 @@ class Login extends StatelessWidget {
                   ),
                   Text(
                     'Compartilhe conquistas, divirta-se e aprenda a ser um programador melhor, todos os dias!',
-                    style: AppTextStyles.body,
+                    style: AppTextStyles.description14,
                     textAlign: TextAlign.center,
                   ),
                   SizedBox(
@@ -96,6 +96,43 @@ class Login extends StatelessWidget {
                               Icons.login,
                               color: Colors.white,
                             ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18),
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        text: "Ao entrar, você concorda com nossos\n",
+                        style: AppTextStyles.description12,
+                        children: [
+                          TextSpan(
+                            text: "Termos de Uso",
+                            style: TextStyle(
+                              color: AppColors.blue,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                _launchURL(
+                                    'https://sites.google.com/view/devpush/termos-de-uso');
+                              },
+                          ),
+                          TextSpan(text: " e "),
+                          TextSpan(
+                            text: "Política de Privacidade",
+                            style: TextStyle(
+                              color: AppColors.blue,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                _launchURL(
+                                    'https://sites.google.com/view/devpush/politica-de-privacidade');
+                              },
                           ),
                         ],
                       ),
