a443c53f6f501987357112a5443eb31e8ff3008a
John Emerson <john.carv.sousa@gmail.com>
2021-05-09 14:43:27 -0300

    feat: add appbar for discover

--- lib/screens/discover_screen/discover_screen.dart
+++ lib/screens/discover_screen/discover_screen.dart
@@ -1,3 +1,4 @@
+import 'package:devpush/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class DiscoverScreen extends StatefulWidget {
@@ -9,10 +10,19 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Discover'),
-        backgroundColor: Colors.pink,
+        title: Text(
+          'Descobrir',
+          style: AppTextStyles.tabTitle,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: ListView(
+        physics: ClampingScrollPhysics(),
+        children: <Widget>[],
       ),
     );
   }
