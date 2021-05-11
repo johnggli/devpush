960af580c1e17c39cb9fb09bbefc97dfbf04e08c
John Emerson <john.carv.sousa@gmail.com>
2021-05-11 18:42:27 -0300

    refactor: change testscreen params

--- lib/screens/discover_screen/discover_screen.dart
+++ lib/screens/discover_screen/discover_screen.dart
@@ -13,6 +13,60 @@ class DiscoverScreen extends StatefulWidget {
 }
 
 class _DiscoverScreenState extends State<DiscoverScreen> {
+  final _questions = const [
+    {
+      'questionText': 'Q1. Who created Flutter?',
+      'answers': [
+        {'text': 'Facebook', 'score': -2},
+        {'text': 'Adobe', 'score': -2},
+        {'text': 'Google', 'score': 10},
+        {'text': 'Microsoft', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q2. What is Flutter?',
+      'answers': [
+        {'text': 'Android Development Kit', 'score': -2},
+        {'text': 'IOS Development Kit', 'score': -2},
+        {'text': 'Web Development Kit', 'score': -2},
+        {
+          'text':
+              'SDK to build beautiful IOS, Android, Web & Desktop Native Apps',
+          'score': 10
+        },
+      ],
+    },
+    {
+      'questionText': ' Q3. Which programing language is used by Flutter',
+      'answers': [
+        {'text': 'Ruby', 'score': -2},
+        {'text': 'Dart', 'score': 10},
+        {'text': 'C++', 'score': -2},
+        {'text': 'Kotlin', 'score': -2},
+      ],
+    },
+    {
+      'questionText': 'Q4. Who created Dart programing language?',
+      'answers': [
+        {'text': 'Lars Bak and Kasper Lund', 'score': 10},
+        {'text': 'Brendan Eich', 'score': -2},
+        {'text': 'Bjarne Stroustrup', 'score': -2},
+        {'text': 'Jeremy Ashkenas', 'score': -2},
+      ],
+    },
+    {
+      'questionText':
+          'Q5. Is Flutter for Web and Desktop available in stable version?',
+      'answers': [
+        {
+          'text': 'Yes',
+          'score': -2,
+        },
+        {'text': 'No', 'score': 10},
+      ],
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +109,9 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => TestScreen(),
+                  builder: (context) => TestScreen(
+                    questions: _questions,
+                  ),
                 ),
               );
             },
--- lib/screens/test_screen/test_screen.dart
+++ lib/screens/test_screen/test_screen.dart
@@ -3,65 +3,17 @@ import 'package:devpush/screens/test_screen/components/test.dart';
 import 'package:flutter/material.dart';
 
 class TestScreen extends StatefulWidget {
+  final List<Map<String, dynamic>> questions;
+  const TestScreen({
+    Key key,
+    @required this.questions,
+  }) : super(key: key);
+
   @override
   _TestScreenState createState() => _TestScreenState();
 }
 
 class _TestScreenState extends State<TestScreen> {
-  final _questions = const [
-    {
-      'questionText': 'Q1. Who created Flutter?',
-      'answers': [
-        {'text': 'Facebook', 'score': -2},
-        {'text': 'Adobe', 'score': -2},
-        {'text': 'Google', 'score': 10},
-        {'text': 'Microsoft', 'score': -2},
-      ],
-    },
-    {
-      'questionText': 'Q2. What is Flutter?',
-      'answers': [
-        {'text': 'Android Development Kit', 'score': -2},
-        {'text': 'IOS Development Kit', 'score': -2},
-        {'text': 'Web Development Kit', 'score': -2},
-        {
-          'text':
-              'SDK to build beautiful IOS, Android, Web & Desktop Native Apps',
-          'score': 10
-        },
-      ],
-    },
-    {
-      'questionText': ' Q3. Which programing language is used by Flutter',
-      'answers': [
-        {'text': 'Ruby', 'score': -2},
-        {'text': 'Dart', 'score': 10},
-        {'text': 'C++', 'score': -2},
-        {'text': 'Kotlin', 'score': -2},
-      ],
-    },
-    {
-      'questionText': 'Q4. Who created Dart programing language?',
-      'answers': [
-        {'text': 'Lars Bak and Kasper Lund', 'score': 10},
-        {'text': 'Brendan Eich', 'score': -2},
-        {'text': 'Bjarne Stroustrup', 'score': -2},
-        {'text': 'Jeremy Ashkenas', 'score': -2},
-      ],
-    },
-    {
-      'questionText':
-          'Q5. Is Flutter for Web and Desktop available in stable version?',
-      'answers': [
-        {
-          'text': 'Yes',
-          'score': -2,
-        },
-        {'text': 'No', 'score': 10},
-      ],
-    },
-  ];
-
   var _questionIndex = 0;
   var _totalScore = 0;
 
@@ -79,7 +31,7 @@ class _TestScreenState extends State<TestScreen> {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < _questions.length) {
+    if (_questionIndex < widget.questions.length) {
       print('We have more questions!');
     } else {
       print('No more questions!');
@@ -95,11 +47,11 @@ class _TestScreenState extends State<TestScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
-        child: _questionIndex < _questions.length
+        child: _questionIndex < widget.questions.length
             ? Test(
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
-                questions: _questions,
+                questions: widget.questions,
               )
             : Result(_totalScore, _resetQuiz),
       ),
