95fc9dd49683c6e38bee0dcd2796901fd5e16ee2
John Emerson <john.carv.sousa@gmail.com>
2021-08-08 18:47:59 -0300

    refactor: change medal_card to only one color

--- lib/screens/profile_screen/components/medal_card.dart
+++ lib/screens/profile_screen/components/medal_card.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'package:devpush/core/app_colors.dart';
-
 class MedalCard extends StatelessWidget {
   final String color;
   final int codePoint;
@@ -23,46 +21,35 @@ class MedalCard extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100),
         border: Border.all(
-          width: 3,
-          color: Colors.yellow,
+          width: 6,
+          color: Color(
+            int.parse(color.split('(0x')[1].split(')')[0], radix: 16),
+          ),
           style: BorderStyle.solid,
         ),
       ),
-      child: Container(
-        height: MediaQuery.of(context).size.width * 0.14,
-        width: MediaQuery.of(context).size.width * 0.14,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: Colors.white,
-          border: Border.all(
-            width: 3,
-            color: AppColors.yellow,
-            style: BorderStyle.solid,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            IconData(codePoint, fontFamily: 'MaterialIcons'),
+            color: Color(
+              int.parse(color.split('(0x')[1].split(')')[0], radix: 16),
+            ),
+            size: 20,
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              IconData(codePoint, fontFamily: 'MaterialIcons'),
+          Text(
+            '$label',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.nunito(
+              fontSize: MediaQuery.of(context).size.width * 0.03,
               color: Color(
                 int.parse(color.split('(0x')[1].split(')')[0], radix: 16),
               ),
-              size: 20,
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              '$label',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.nunito(
-                fontSize: MediaQuery.of(context).size.width * 0.03,
-                color: Color(
-                  int.parse(color.split('(0x')[1].split(')')[0], radix: 16),
-                ),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
