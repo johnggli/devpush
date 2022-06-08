aa8003782f29af119b9b55a619b9eaf69b76f880
John Emerson <john.carv.sousa@gmail.com>
2022-06-08 18:47:45 -0300

    feat: add NeverScrollableScrollPhysics to tab nav

--- lib/screens/quiz_list_screen/quiz_list_screen.dart
+++ lib/screens/quiz_list_screen/quiz_list_screen.dart
@@ -69,6 +69,7 @@ class _QuizListScreenState extends State<QuizListScreen> {
           ),
         ),
         body: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
           children: [
             StreamBuilder<QuerySnapshot>(
               stream: databaseProvider.getAllQuizzes(),
